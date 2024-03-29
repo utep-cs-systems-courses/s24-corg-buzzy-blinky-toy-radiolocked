bd948b108c6b6037e212212f574bc130b51e1942
radiolocked <dilancl808@gmail.com>
2024-03-29 16:25:40 -0600

    <Suggested activity done for 1-led>

--- demos/1-led/main.c
+++ demos/1-led/main.c
@@ -2,11 +2,16 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"
+#include <stdio.h>
 
 int main(void) {
-  P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+  P1DIR |= LEDS; //sets the direction of port 1 to LEDS
+  //original demo code
+  //P1OUT &= ~LED_GREEN; //&=~ will always set all bits to 0 (so LED is being turned off)
+  //P1OUT |= LED_RED; //LED is being turned on
 
+  //modified code to turn both leds on
+  P1OUT |= LED_GREEN;
+  P1OUT |= LED_RED;
   or_sr(0x18);		/* CPU off, GIE on */
 }
