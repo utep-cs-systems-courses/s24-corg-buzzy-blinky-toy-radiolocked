0f061104c9ba63b93294438e6930b66728716ca5
radiolocked <dilancl808@gmail.com>
2024-03-30 16:50:42 -0600

    <Made both leds have a different dimming pattern and also changeed speed>

--- demos/4-led-blink-pattern/main.c
+++ demos/4-led-blink-pattern/main.c
@@ -15,28 +15,45 @@ int main(void) {
 }
 
 // global state vars that control blinking
-int blinkLimit = 5;  // duty cycle = 1/blinkLimit
-int blinkCount = 0;  // cycles 0...blinkLimit-1
+int blinkLimitG = 0;  // duty cycle = 1/blinkLimit
+int blinkLimitR = 10;
+int blinkCountG = 0;  // cycles 0...blinkLimit-1
+int blinkCountR = 0;
 int secondCount = 0; // state var representing repeating time 0…1s
 
+
+//i added a pattern to the red light. it goes dimmer while the green led goes brighter
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
   // handle blinking 
-  blinkCount ++;
-  if (blinkCount >= blinkLimit) { // on for 1 interrupt period
-    blinkCount = 0;
+  blinkCountG ++;
+  blinkCountR ++;
+  if (blinkCountG >= blinkLimitG) { // on for 1 interrupt period
+    blinkCountG = 0;
     P1OUT |= LED_GREEN;
-  } else		          // off for blinkLimit - 1 interrupt periods
+  } else {		          // off for blinkLimit - 1 interrupt periods
     P1OUT &= ~LED_GREEN;
+  }
+  if(blinkCountR >= blinkLimitR){
+    blinkCountR = 0;
+    P1OUT |= LED_RED;
+  }else{
+    P1OUT &= ~LED_RED;
+  }
 
   // measure a second
   secondCount ++;
-  if (secondCount >= 250) {  // once each second
+  if (secondCount >= 125) {  // once each second
     secondCount = 0;
-    blinkLimit ++;	     // reduce duty cycle
-    if (blinkLimit >= 8)     // but don't let duty cycle go below 1/7.
-      blinkLimit = 0;
+    blinkLimitR ++;
+    blinkLimitG --;	     // increase duty cycle (changed from reduce)
+    if (blinkLimitG <= 0){     // set limit to duty cycle
+      blinkLimitG = 10;
+    }
+    if (blinkLimitR >= 10){
+      blinkLimitR = 0;
+    }
   }
 } 
 
