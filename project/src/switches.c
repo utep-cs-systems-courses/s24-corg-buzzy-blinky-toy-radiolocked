f0493b44ef5cf5ced691bc57b03d2fa93e67237b
radiolocked <dilancl808@gmail.com>
2024-04-15 19:59:14 -0600

    pushing for safety

--- project/src/switches.c
+++ project/src/switches.c
@@ -1,6 +1,13 @@
 #include <msp430.h>
 #include "switches.h"
+#include "libTimer.h"
 #include "led.h"
+
+//global state variables that control blinking
+int blinkLimit = 0;
+int blinkCount = 0;
+int secondCount = 0;
+int buttonPressed = -1;
 
 void
 switch_init()
@@ -22,10 +29,39 @@ switch_interrupt_handler()
 
   /* up=red, down=green */
   if (p2val & S1) {
+    buttonPressed = 0;
     if(!(P1OUT & LEDS)){//this line checks if both leds is off
       P1OUT |= LED_GREEN;//if both lights are of (the start) only turn on the green led
     }else{//once one led is on, then the button makes them alternate
       P1OUT ^= LEDS;
+    }
+  }
+  if(p2val & S2){
+    buttonPressed = 1;
+    blinkLimit--;
+  }
+}
+
+//Pattens for leds blinking
+void
+__interrupt_vec(WDT_VECTOR) WDT()/* 250 interrupts/sec */
+{
+  if(buttonPressed == 1){
+    blinkCount++;
+    if(blinkCount >= blinkLimit){
+      blinkCount = 0;
+      P1OUT ^= LED_GREEN;
+    }else{
+      P1OUT ^= LED_GREEN;
+    }
+
+    //measure half a second
+    secondCount++;
+    if(secondCount >= 125){
+      secondCount = 0;
+      if(blinkLimit <= 0){//set limit to duty cycle
+	blinkLimit = 5;
+      }
     }
   }
 }
--- project/src/switches.h
+++ project/src/switches.h
@@ -1,8 +1,9 @@
 #ifndef switches_included
 #define switches_included
 
-#define S1 BIT0 //switch is p1.3
-#define SWITCHES S1 //only 1 switch on this board
+#define S1 BIT0 //switch is p2.0
+#define S2 BIT1 //switch is in p2.1
+#define SWITCHES S1, S2//only 1 switch on this board
 
 void switch_init();
 void switch_interrupt_handler();
